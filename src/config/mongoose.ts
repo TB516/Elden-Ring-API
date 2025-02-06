3265b50650c769b0fd85ece304e61735d51a2379
Thomas <77514919+TB516@users.noreply.github.com>
2025-02-05 21:27:07 -0500

    removed unused import

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,16 +1,22 @@
-import mongoose, { model } from 'mongoose';
-import { container } from 'tsyringe';
-import { Spell } from '../models/spells';
-import SpellRepository from '../repositories/mongo/spell.repository';
-import IRepository from '../repositories/repository.interface';
-import Sorceries from '../db/mongo/spells/sorceries.mongo-model';
-import Incantations from '../db/mongo/spells/incantations.mongo-model';
+import mongoose from "mongoose";
+import { container } from "tsyringe";
+import { Spell } from "../models/spells";
+import SpellRepository from "../repositories/mongo/spell.repository";
+import IRepository from "../repositories/repository.interface";
+import Sorceries from "../db/mongo/spells/sorceries.mongo-model";
+import Incantations from "../db/mongo/spells/incantations.mongo-model";
 
 const init = async () => {
   await mongoose.connect(process.env.MONGODB_URI!);
 
-  container.registerInstance<IRepository<Spell>>('SorceriesRepo', new SpellRepository(Sorceries));
-  container.registerInstance<IRepository<Spell>>('IncantationsRepo', new SpellRepository(Incantations));
+  container.registerInstance<IRepository<Spell>>(
+    "SorceriesRepo",
+    new SpellRepository(Sorceries)
+  );
+  container.registerInstance<IRepository<Spell>>(
+    "IncantationsRepo",
+    new SpellRepository(Incantations)
+  );
 };
 
 export default init;
