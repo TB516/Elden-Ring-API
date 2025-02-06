23f0d8b52d9270a1999d9f32050d45d52b32e63e
Thomas <77514919+TB516@users.noreply.github.com>
2025-02-06 00:47:21 -0500

    Fixed jestconfig esm error
    Resolved issue with ts-jest config

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export const testEnvironment = "node";
-export const rootDir = "tests";
-export const transform = {
-  "^.+.tsx?$": ["ts-jest", {}],
-};
